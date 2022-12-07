cc35f3d16a74250b2cdad5adf4fac8543635b80a
Rick Segrest <rsegrest77@gmail.com>
2022-12-07 00:22:56 -0600

    Set up basic necessary files for Saucer. Need to add shooting, and test drawing and positoning... start with a straight line.

--- src/draw.ts
+++ src/draw.ts
@@ -2,14 +2,6 @@ import type p5 from "p5";
 import GameController from "./controller/GameController";
 
 import {
-  // pShip,
-  // asteroidController,
-  font,
-  // scoreDisplay,
-  // livesDisplay,
-  // playerController,
-  // playerDisplay,
-  // footerDisplay,
   gameController,
 } from "./setup";
 
@@ -18,6 +10,5 @@ import {
 export const draw = (p: p5): void => {
   p.createCanvas(800,600);
   p.background(10);
-  // addFooter(p,font);
   gameController.advance();
 };
---
+++ src/view/SaucerDisplay.ts
@@ -0,0 +1,30 @@
+import p5 from 'p5';
+import SaucerModel from '../model/SaucerModel';
+class SaucerDisplay {
+  private static p:p5;
+  constructor(p:p5) {
+    SaucerDisplay.p = p;
+  }
+  static draw(saucer:SaucerModel, scale:number) {
+    // larger size space ship
+    const drawSpaceShip = (p:p5) => {
+      p.push();
+      p.stroke(0,255,0);
+      p.strokeWeight(1);
+      p.translate(saucer.getX(), saucer.getY());
+      p.scale(scale);
+      p.noFill();
+      p.quad(-55,7,55,7,25,22,-25,22);
+      p.quad(-55,7,55,7,20,-10,-20,-10);
+      p.quad(20,-10,-20,-10,-10,-30,10,-30);
+      p.pop();
+    }
+  }
+  static drawLargeSaucer(saucer:SaucerModel) {
+    this.draw(saucer, 1.5);
+  }
+  static drawSmallSaucer(saucer:SaucerModel) {
+    this.draw(saucer, 0.7)
+  }
+}
+export default SaucerDisplay;
